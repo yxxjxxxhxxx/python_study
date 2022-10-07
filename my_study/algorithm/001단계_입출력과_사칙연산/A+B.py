ddda5452bd13c7eefac7b6a956140a03dd7f75f0
yzzjzzzhzzz <whdgus3012@naver.com>
2022-10-07 20:49:40 +0900

    python_221007

---
+++ my_study/algorithm/001단계_입출력과_사칙연산/A+B.py
@@ -0,0 +1,29 @@
+#1000
+'''
+A = int(input("A정수입력 : "))
+B = int(input("B정수입력 : "))
+print(A+B)
+'''
+
+'''
+A = int(input("A 정수 입력: "))
+B = int(input("B 정수 입력: "))
+C = A+B
+print(C)
+'''
+
+'''
+C, D = 5, 6
+
+print(C)
+print(D)
+
+A, B = input("숫자 하나를 입력하고 SPACEBAR 입력후 숫자 하나를 입력하세요\n").split()
+
+print(int(A)+int(B))
+'''
+
+A, B = int(input()), int(input())
+print(A+B)
+
+
